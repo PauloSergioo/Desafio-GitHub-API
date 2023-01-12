820e759c21e0a1c1030162a3c2f84494eea9ebb1
Paulo Sergio <paulosergioilha38@gmail.com>
2023-01-12 12:11:45 -0300

    small changes

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,7 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Button from "../Button";
 import "./styles.css";
 
+type FormData = {
+  profileName: string;
+};
+
 export default function Search() {
+  const [nameData, setNameData] = useState<string>();
+
+  const [formData, setFormData] = useState<FormData>({
+    profileName: "",
+  });
+
+  function handleProfileName(event: any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleFormSubmit(event: any) {
+    event.preventDefault();
+    setNameData(formData.profileName);
+  }
+
+  useEffect(() => {
+    axios.get(`https://api.github.com/users/${formData.profileName}`)
+    .then(response => {
+      setNameData(response.data)
+      console.log(formData.profileName)
+    })
+  }, []);
+
   return (
     <main>
       <section>
@@ -10,11 +42,19 @@ export default function Search() {
             <h2>Encontre um perfil Github</h2>
           </div>
           <div className="search-form">
-            <form>
-              <input type="text" placeholder="Usuário Github" />
+            <form onSubmit={handleFormSubmit}>
+              <input
+                name="profileName"
+                value={formData.profileName}
+                type="text"
+                placeholder="Usuário Github"
+                onChange={handleProfileName}
+              />
             </form>
           </div>
-          <Button name="Encontrar" />
+          <Link  to={"info"}>
+            <Button name="Encontrar" />
+          </Link>
         </div>
       </section>
     </main>
